c794d8d2e761e535d622ad1c31d51f5fc8319574
MisbahDayo <dayomuhammad892@gmail.com>
2024-06-26 23:02:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// TUPLES IN TYPESCRIPT
+//A TUPLES IS A TYPED ARRAY
+// WITH A PRE DEFINED LENGTH
+// AND TYPES FOR EACH INDEX
+var arr = ["Misbah", 500, true];
+arr.push("jameel");
+console.log(arr);
---
+++ main.ts
@@ -0,0 +1,15 @@
+// TUPLES IN TYPESCRIPT
+
+//A TUPLES IS A TYPED ARRAY
+// WITH A PRE DEFINED LENGTH
+// AND TYPES FOR EACH INDEX
+
+let arr: [string,number,boolean,{name:string}?] =
+
+
+["Misbah",500,true];
+
+
+arr.push("jameel");
+
+console.log(arr);
